127a86471a0cb136d469f0fdfa498cb69fb1c617
Luisa Verza <luisaverza@gmail.com>
2017-05-23 16:09:10 -0300

    express server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.use('/', (req, res) => {
+	res.status(200).send('Hello World');
+});
+
+app.listen(3000);
+console.log('Server running');
+
+module.exports = app;
