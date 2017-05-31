22e9c321234dcdeb93175380f88116d857a17804
Luisa Verza <luisaverza@gmail.com>
2017-05-31 09:32:15 -0300

    1. instalei o angular através do nem install angular --save (versão 1.6.4) 2. adicionei app.use('/lib', express.static(path.resolve('./node_modules'))); no arquivo do express.js, para definir que quando for acessado caminho da lib, irá para node_modules 	2.1 adicionei const path = require('path'); no inicio do express.js 3. adicionei o script <script src="lib/angular/angular.min.js"></script> na index.ejs

--- config/express.js
+++ config/express.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const config = require('./config');
 const express = require('express');
 const morgan = require('morgan');
@@ -9,40 +10,43 @@ const flash = require('connect-flash');
 const passport = require('passport');
 
 module.exports = function() {
-const app = express();
+	const app = express();
 
-if (process.env.NODE_ENV === 'development') {
-	app.use(morgan('dev'));
-} else if (process.env.NODE_ENV === 'production') {
-	app.use(compress());
-}
+	if (process.env.NODE_ENV === 'development') {
+		app.use(morgan('dev'));
+	} else if (process.env.NODE_ENV === 'production') {
+		app.use(compress());
+	}
 
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
+	app.use(bodyParser.urlencoded({
+		extended: true
+	}));
 
-app.use(bodyParser.json());
+	app.use(bodyParser.json());
 
-app.use(methodOverride());
+	app.use(methodOverride());
 
-app.use(session({
-	saveUninitialized: true,
-	resave: true,
-	secret: config.sessionSecret
-}));
+	app.use(session({
+		saveUninitialized: true,
+		resave: true,
+		secret: config.sessionSecret
+	}));
 
-app.set('views', './app/views');
-app.set('view engine', 'ejs');
+	app.set('views', './app/views');
+	app.set('view engine', 'ejs');
 
-app.use(flash());
+	app.use(flash());
 
-app.use(passport.initialize());
+	app.use(passport.initialize());
 
-app.use(passport.session());
+	app.use(passport.session());
 
-require('../app/routes/index.server.routes.js')(app);
-require('../app/routes/users.server.routes.js')(app);
+	require('../app/routes/index.server.routes.js')(app);
+	require('../app/routes/users.server.routes.js')(app);
 
-app.use(express.static('./public'));
+	app.use('/', express.static(path.resolve('./public')));
+	app.use('/lib', express.static(path.resolve('./node_modules')));
+
 	return app;
 };
+                                                                                                                  
